5831b0ba27539c7a606cdaf9fd55a96c2c3c9a96
Yannik Sander <yannik@floxdev.com>
2022-09-12 18:36:29 +0200

    Make it build

---
+++ pkgs/default.nix
@@ -0,0 +1,18 @@
+# Replace "stdenv" with the namespace or name of your language's builder
+{stdenv}:
+# Replace "stdenv.mkDerivation" with your language's builder
+stdenv.mkDerivation {
+  name = "my-package";
+  src = ../.;
+
+  installPhase = ''
+    export PREFIX="$out"
+    make install
+  '';
+
+  # Add any dependencies your software needs at runtime to propagatedBuildInputs
+  propagatedBuildInputs = [];
+
+  # Add any dependencies your software only needs at buildtime to buildInputs
+  buildInputs = [];
+}
